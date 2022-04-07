db37c4f497e6e9560f6d79d7a3ebc0840c7e7ab3
sm-jahangir <samraatjahangir@gmail.com>
2022-04-07 23:08:04 +0600

    Category Done

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
@@ -15,7 +17,9 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        // $categories = Category::where('parent_id', null)->orderBy('order', 'DESC')->get();
+        $categories = Category::orderBy('order', 'DESC')->get();
+        return view('backend.category.index', compact('categories'));
     }
 
     /**
@@ -25,7 +29,8 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        $parent_category = Category::where('parent_id', 0)->orderBy('order', 'DESC')->get();
+        return view('backend.category.form', compact('parent_category'));
     }
 
     /**
@@ -36,7 +41,26 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // return $request;
+        $category = Category::create([
+            'name' => $request->name,
+            'parent_id' => $request->parent_category,
+            'slug' => Str::slug($request->name),
+            'status' => $request->filled('status'),
+        ]);
+        // upload images
+
+        if ($request->has('image')) {
+            $img = $request->file('image');
+            $ext = $img->extension();
+            $file = time() . '.' . $ext;
+            $img->storeAs('public/category', $file); //above 4 line process the image code
+            $category->image =  $file; //ai code ta image ke insert kore
+        }
+        $category->save();
+
+        notify()->success('Category Successfully Added.', 'Added');
+        return redirect()->route('app.category.index');
     }
 
     /**
@@ -58,7 +82,8 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        $parent_category = Category::where('parent_id', 0)->orderBy('order', 'DESC')->get();
+        return view('backend.category.form', compact('category', 'parent_category'));
     }
 
     /**
@@ -70,7 +95,27 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        // return $request;
+        $category->update([
+            'name' => $request->name,
+            'parent_id' => $request->parent_category,
+            'status' => $request->filled('status'),
+        ]);
+        // upload images
+        if ($request->hasFile('image') && request('image') != '') {
+            $imagePath = public_path('storage/category/' . $category->image);
+            if (File::exists($imagePath)) {
+                unlink($imagePath);
+            }
+            $img = $request->file('image');
+            $ext = $img->extension();
+            $file = time() . '.' . $ext;
+            $img->storeAs('public/category', $file); //above 4 line process the image code
+            $category->image =  $file; //ai code ta image ke insert kore
+            $category->save();
+        }
+        notify()->success('Category Updated Successfully.', 'Updated');
+        return redirect()->route('app.category.index');
     }
 
     /**
@@ -81,6 +126,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        notify()->success("Category Successfully Deleted", "Deleted");
+        return back();
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(PageSeeder::class);
+        $this->call(CategorySeeder::class);
     }
 }
