9a18210bdc15ce44a63f190fb67ea9fd7e99f208
sm-jahangir <samraatjahangir@gmail.com>
2022-04-28 03:36:01 +0600

    Product CRUD operation done

--- app/Http/Controllers/backend/ProductController.php
+++ app/Http/Controllers/backend/ProductController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Backend;
 
 use App\Models\Tag;
 use App\Models\Size;
+use App\Models\User;
 use App\Models\Brand;
 use App\Models\Color;
+use App\Models\Image;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Image;
-use App\Models\User;
+use Illuminate\Support\Facades\File;
 
 class ProductController extends Controller
 {
@@ -139,7 +140,20 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        $categories = Category::latest()->get();
+        $tags = Tag::latest()->get();
+        $colors = Color::latest()->get();
+        $sizes = Size::latest()->get();
+        $brands = Brand::latest()->get();
+        // $users = User::where('role_id', 1 || 'role_id', 2 && 'status', 1)->andWhere('status', true)->get();
+        $users = User::where(function ($query) {
+            $query->where('role_id', 1)
+                ->orWhere('role_id', 2);
+        })->where(function ($query) {
+            $query->where('status', true);
+        })->get();
+
+        return view('backend.product.form', compact('product', 'categories', 'tags', 'colors', 'sizes', 'brands', 'users'));
     }
 
     /**
@@ -151,7 +165,71 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+
+        // return $request->all();
+        $this->validate($request, [
+            'title' =>  'required|string',
+            'description'  =>  'required|string',
+            'price'  =>  'required|string',
+            'featured_image' =>  'nullable|image'
+        ]);
+        $product->update([
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+            'excerpt' => $request->excerpt,
+            'description' => $request->description,
+            'sku' => $request->sku,
+            'user_id' => $request->user_id,
+            'brand_id' => $request->brand_id,
+
+            'product_collections' => $request->product_collections,
+            'labels' => $request->labels,
+            'weight' => $request->weight,
+            'height' => $request->height,
+            'length' => $request->length,
+            'wide' => $request->wide,
+            'price' => $request->price,
+            'sale_price' => $request->sale_price,
+
+            'meta_description' => $request->meta_description,
+            'meta_keywords' => $request->meta_keywords,
+            'featured' => $request->filled('featured'),
+            'trending' => $request->filled('trending'),
+            'popular' => $request->filled('popular'),
+            'status' => $request->filled('status'),
+        ]);
+        // upload images
+        if ($request->hasFile('featured_image')) {
+            $imagePath = public_path('storage/products/' . $product->featured_image);
+            if (File::exists($imagePath)) {
+                unlink($imagePath);
+            }
+
+            $image = $request->file('featured_image');
+            $ext = $image->extension();
+            $file = time() . '.' . $ext;
+            $image->storeAs('public/products', $file); //above 4 line process the image code
+            $product->featured_image =  $file; //ai code ta image ke insert kore
+            $product->update();
+        }
+        if ($request->has('images')) {
+            foreach ($request->file('images') as $key => $image) {
+                $imageName = $request->title . '-image-' . time() . rand(1, 1000) . '.' . $image->extension();
+                $image->storeAs('public/product_gallery', $imageName);
+                Image::create([
+                    'product_id' => $product->id,
+                    'image' => $imageName
+                ]);
+            }
+        }
+
+        $product->categories()->sync($request->categories);
+        $product->tags()->sync($request->tags);
+        $product->colors()->sync($request->colors);
+        $product->sizes()->sync($request->sizes);
+
+        notify()->success('Product Successfully Updated.', 'Updated');
+        return redirect()->route('app.product.index');
     }
 
     /**
@@ -162,6 +240,15 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $image_path = public_path("\storage\products\\") . $product->featured_image;
+        if (File::exists($image_path)) {
+            File::delete($image_path);
+        } else {
+            $product->delete();
+            //abort(404);
+        }
+        $product->delete();
+        notify()->success("Portfolio Successfully Deleted", "Deleted");
+        return back();
     }
 }
--- resources/views/backend/product/form.blade.php
+++ resources/views/backend/product/form.blade.php
@@ -90,22 +90,45 @@
 									</div>
 									<hr>
 									<div class="bs-stepper-content">
-										<form action="{{ route('app.product.store') }}" method="POST" enctype="multipart/form-data">
+										<form action="{{ isset($product) ? route('app.product.update', $product->id) : route('app.product.store') }}"
+											method="POST" enctype="multipart/form-data">
+
+											@isset($product)
+												@method('PUT')
+											@endisset
 											@csrf
 											<!-- General steps content here -->
 											<div id="general-part" class="content" role="tabpanel" aria-labelledby="logins-part-trigger">
 												<div class="form-group">
 													<label for="productInputTitle">Product Title</label>
-													<input type="text" class="form-control" name="title" id="productInputTitle"
+													<input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+														id="productInputTitle" value="{{ isset($product) ? $product->title : old('title') }}" required
 														placeholder="Enter Product Title" />
+													@error('title')
+														<span class="invalid-feedback" role="alert">
+															<strong>{{ $message }}</strong>
+														</span>
+													@enderror
 												</div>
 												<div class="form-group">
 													<label for="productShortDescription">Short Description</label>
-													<textarea class="form-control" name="excerpt" id="productShortDescription" cols="30" rows="5"></textarea>
+													<textarea class="form-control @error('excerpt') is-invalid @enderror" name="excerpt" id="productShortDescription"
+              cols="30" rows="5">{{ isset($product) ? $product->excerpt : old('excerpt') }}</textarea>
+													@error('excerpt')
+														<span class="invalid-feedback" role="alert">
+															<strong>{{ $message }}</strong>
+														</span>
+													@enderror
 												</div>
 												<div class="form-group">
 													<label for="productDescription">Description</label>
-													<textarea class="form-control" name="description" id="productDescription" cols="30" rows="5"></textarea>
+													<textarea class="form-control @error('description') is-invalid @enderror" name="description" id="productDescription"
+              cols="30" rows="5">{{ isset($product) ? $product->description : old('description') }}</textarea>
+													@error('description')
+														<span class="invalid-feedback" role="alert">
+															<strong>{{ $message }}</strong>
+														</span>
+													@enderror
 												</div>
 												<button type="button" class="btn btn-primary" onclick="stepper.next()">Next</button>
 											</div>
@@ -115,18 +138,29 @@
 													<div class="col-md-6">
 														<div class="form-group">
 															<label>Product SKU</label>
-															<input class="form-control" placeholder="Enter Your SKU" type="text" name="sku" id="sku">
+															<input class="form-control @error('sku') is-invalid @enderror" placeholder="Enter Your SKU" type="text"
+																name="sku" id="sku" value="{{ isset($product) ? $product->sku : old('sku') }}" required>
+															@error('sku')
+																<span class="invalid-feedback" role="alert">
+																	<strong>{{ $message }}</strong>
+																</span>
+															@enderror
 														</div>
 													</div>
 
 													<div class="col-md-6">
 														<div class="form-group">
 															<label>Categories</label>
-															<select name="categories" class="select2 select2-hidden-accessible" multiple=""
+															<select name="categories[]" class="select2 select2-hidden-accessible" multiple=""
 																data-placeholder="Select a Color" style="width: 100%;" data-select2-id="13" tabindex="-1"
 																aria-hidden="true">
 																@foreach ($categories as $category)
-																	<option value="{{ $category->id }}">{{ $category->name }}</option>
+																	<option
+																		@isset($product) @foreach ($product->categories as $product_cat)
+                                                                    		{{ $product_cat->id == $category->id ? 'selected' : '' }}
+
+                                                                		@endforeach @endisset
+																		value="{{ $category->id }}">{{ $category->name }}</option>
 																@endforeach
 															</select>
 														</div>
@@ -135,11 +169,16 @@
 													<div class="col-md-6">
 														<div class="form-group">
 															<label>Tags</label>
-															<select name="tags" class="select2 select2-hidden-accessible" multiple=""
+															<select name="tags[]" class="select2 select2-hidden-accessible" multiple=""
 																data-placeholder="Select a Color" style="width: 100%;" data-select2-id="12" tabindex="-1"
 																aria-hidden="true">
 																@foreach ($tags as $tag)
-																	<option value="{{ $tag->id }}">{{ $tag->name }}</option>
+																	<option
+																		@isset($product) @foreach ($product->tags as $product_tag)
+                                                                    		{{ $product_tag->id == $tag->id ? 'selected' : '' }}
+
+                                                                		@endforeach @endisset
+																		value="{{ $tag->id }}">{{ $tag->name }}</option>
 																@endforeach
 															</select>
 														</div>
@@ -150,7 +189,9 @@
 															<label>Brand</label>
 															<select name="brand_id" class="custom-select" style="width: 100%;">
 																@foreach ($brands as $brand)
-																	<option value="{{ $brand->id }}">{{ $brand->name }}</option>
+																	<option
+																		@isset($product) {{ $product->brand_id == $brand->id ? 'selected' : '' }} @endisset
+																		value="{{ $brand->id }}">{{ $brand->name }}</option>
 																@endforeach
 															</select>
 														</div>
@@ -164,7 +205,12 @@
 																data-placeholder="Select a Color" style="width: 100%;" data-select2-id="8" tabindex="-1"
 																aria-hidden="true">
 																@foreach ($colors as $color)
-																	<option value="{{ $color->id }}">{{ $color->name }}</option>
+																	<option
+																		@isset($product) @foreach ($product->colors as $product_color)
+																	{{ $product_color->id == $color->id ? 'selected' : '' }}
+
+																@endforeach @endisset
+																		value="{{ $color->id }}">{{ $color->name }}</option>
 																@endforeach
 															</select>
 														</div>
@@ -176,7 +222,12 @@
 																data-placeholder="Select a Color" style="width: 100%;" data-select2-id="9" tabindex="-1"
 																aria-hidden="true">
 																@foreach ($sizes as $size)
-																	<option value="{{ $size->id }}">{{ $size->name }}</option>
+																	<option
+																		@isset($product) @foreach ($product->sizes as $product_size)
+																	{{ $product_size->id == $size->id ? 'selected' : '' }}
+
+																@endforeach @endisset
+																		value="{{ $size->id }}">{{ $size->name }}</option>
 																@endforeach
 															</select>
 														</div>
@@ -187,9 +238,15 @@
 														<div class="form-group">
 															<label>Product Collection</label>
 															<select name="product_collections" class="custom-select">
-																<option>New Arrival</option>
-																<option>Best Sellers</option>
-																<option>Special Offer</option>
+																<option
+																	@isset($product) {{ $product->product_collections == 'New Arrival' ? 'selected' : '' }} @endisset>
+																	New Arrival</option>
+																<option
+																	@isset($product) {{ $product->product_collections == 'Best Sellers' ? 'selected' : '' }} @endisset>
+																	Best Sellers</option>
+																<option
+																	@isset($product) {{ $product->product_collections == 'Special Offer' ? 'selected' : '' }} @endisset>
+																	Special Offer</option>
 															</select>
 														</div>
 													</div>
@@ -197,9 +254,15 @@
 														<div class="form-group">
 															<label>Label</label>
 															<select name="labels" class="custom-select">
-																<option>Hot</option>
-																<option>New</option>
-																<option>Sale</option>
+																<option
+																	@isset($product) {{ $product->labels == 'Hot' ? 'selected' : '' }} @endisset>Hot
+																</option>
+																<option
+																	@isset($product) {{ $product->labels == 'New' ? 'selected' : '' }} @endisset>New
+																</option>
+																<option
+																	@isset($product) {{ $product->labels == 'Sale' ? 'selected' : '' }} @endisset>
+																	Sale</option>
 															</select>
 														</div>
 													</div>
@@ -208,13 +271,15 @@
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="weight">Product Weight</label>
-															<input type="text" class="form-control" name="weight" id="weight" placeholder="Enter Weight in Gram">
+															<input type="text" class="form-control" name="weight" id="weight" placeholder="Enter Weight in Gram"
+																value="{{ isset($product) ? $product->weight : old('weight') }}" required>
 														</div>
 													</div>
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="height">Height</label>
-															<input type="text" class="form-control" name="height" id="height" placeholder="Enter height in cm">
+															<input type="text" class="form-control" name="height" id="height" placeholder="Enter height in cm"
+																value="{{ isset($product) ? $product->height : old('height') }}" required>
 														</div>
 													</div>
 												</div>
@@ -222,13 +287,15 @@
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="length">Product length</label>
-															<input type="text" class="form-control" name="length" id="length" placeholder="Enter length in cm">
+															<input type="text" class="form-control" name="length" id="length" placeholder="Enter length in cm"
+																value="{{ isset($product) ? $product->length : old('length') }}" required>
 														</div>
 													</div>
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="wide">wide</label>
-															<input type="text" class="form-control" name="wide" id="wide" placeholder="Enter wide in cm">
+															<input type="text" class="form-control" name="wide" id="wide" placeholder="Enter wide in cm"
+																value="{{ isset($product) ? $product->wide : old('wide') }}" required>
 														</div>
 													</div>
 												</div>
@@ -242,14 +309,16 @@
 														<div class="form-group">
 															<label for="productInputPrice">Product Price</label>
 															<input type="text" class="form-control" name="price" id="productInputPrice"
-																placeholder="Enter Product Price" />
+																placeholder="Enter Product Price" value="{{ isset($product) ? $product->price : old('price') }}"
+																required>
 														</div>
 													</div>
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="productInputSalePrice">Product Sale Price</label>
 															<input type="text" class="form-control" name="sale_price" id="productInputSalePrice"
-																placeholder="Enter Product Sale Price" />
+																placeholder="Enter Product Sale Price"
+																value="{{ isset($product) ? $product->sale_price : old('sale_price') }}" required>
 														</div>
 													</div>
 												</div>
@@ -261,7 +330,8 @@
 
 												<div class="form-group">
 													<label for="image">Product Image(Optional)</label>
-													<input type="file" name="featured_image" id="image" class="dropify" data-default-file="">
+													<input type="file" name="featured_image" id="image" class="dropify"
+														@isset($product) data-default-file="{{ asset('storage/products') . '/' . $product->featured_image }}" @endisset>
 												</div>
 												<div class="form-group">
 													<label for="gallery">Product Gallery (Optional)</label>
@@ -281,14 +351,16 @@
 														<div class="form-group">
 															<label for="meta_keywords">Meta Keywords</label>
 															<input type="text" class="form-control" name="meta_keywords" id="meta_keywords"
-																placeholder="Enter Product Price" />
+																placeholder="Enter Product Price"
+																value="{{ isset($product) ? $product->meta_keywords : old('meta_keywords') }}" required>
 														</div>
 													</div>
 													<div class="col-md-6">
 														<div class="form-group">
 															<label for="meta_description">Meta Description</label>
 															<input type="text" class="form-control" name="meta_description" id="meta_description"
-																placeholder="Enter Product Sale Price" />
+																placeholder="Enter Product Sale Price"
+																value="{{ isset($product) ? $product->meta_description : old('meta_description') }}" required>
 														</div>
 													</div>
 												</div>
@@ -300,27 +372,32 @@
 													<div class="col-md-3">
 														<label for="featured">Featured Product</label>
 														<input type="checkbox" name="featured" data-bootstrap-switch data-off-color="danger"
-															data-on-color="success">
+															data-on-color="success"
+															@isset($product) {{ $product->featured == true ? 'checked' : '' }} @endisset>
 													</div>
 													<div class="col-md-3">
 														<label for="trending">trending Product</label>
 														<input type="checkbox" name="trending" data-bootstrap-switch data-off-color="danger"
-															data-on-color="success">
+															data-on-color="success"
+															@isset($product) {{ $product->trending == true ? 'checked' : '' }} @endisset>
 													</div>
 													<div class="col-md-3">
 														<label for="popular">popular Product</label>
-														<input type="checkbox" name="popular" data-bootstrap-switch data-off-color="danger"
-															data-on-color="success">
+														<input type="checkbox" name="popular" data-bootstrap-switch data-off-color="danger" data-on-color="success"
+															@isset($product) {{ $product->popular == true ? 'checked' : '' }} @endisset>
 													</div>
 													<div class="col-md-3">
 														<label for="featured">Product Status</label>
-														<input type="checkbox" name="status" checked data-bootstrap-switch>
+														<input type="checkbox" name="status" data-bootstrap-switch
+															@isset($product) {{ $product->status == true ? 'checked' : '' }} @endisset>
 													</div>
 													<div class="col-md-12 d-flex mt-3">
 														<label class="mr-2">Shop</label>
 														<select name="user_id" class="custom-select w-50">
 															@foreach ($users as $user)
-																<option value="{{ $user->id }}">{{ $user->name }}</option>
+																<option
+																	@isset($product) {{ $product->user_id == $user->id ? 'selected' : '' }} @endisset
+																	value="{{ $user->id }}">{{ $user->name }}</option>
 															@endforeach
 														</select>
 													</div>
