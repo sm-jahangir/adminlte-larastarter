655ed20051ccfe314881333a748ef4e3a42cae2e
sm-jahangir <samraatjahangir@gmail.com>
2022-06-16 10:42:48 +0600

    login and register page cdn link added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,17 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('mainContent')
-	<div class="container">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+</head>
+
+<body>
+	<div class="container mt-5">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
 				<div class="card">
@@ -68,10 +78,19 @@
 									</button>
 								</div>
 							</div>
+
+							<div class="d-flex gap-3 m-auto">
+								<p class="mr-2">Already Have an Account</p><a href="{{ url('login') }}">Login</a>
+							</div>
 						</form>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-@endsection
+	<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</body>
+
+</html>
